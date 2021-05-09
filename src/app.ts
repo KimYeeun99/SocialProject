72e983c1da34445433a7cbf54b74b81265fc7ddb
KimYeeun99 <kyz0608@naver.com>
2021-05-09 13:46:46 +0900

    fix/register

--- src/app.ts
+++ src/app.ts
@@ -13,34 +13,34 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 declare module "express-session" {
-    interface SessionData {
-        userId: String;
-        password: String;
-        isLogedIn: boolean;
-    }
+  interface SessionData {
+    userId: String;
+    password: String;
+    isLogedIn: boolean;
+  }
 }
 
 app.use(
-    session({
-        secret: "asadlfkj!@#!@#dfgasdg",
-        resave: false,
-        saveUninitialized: true,
-    })
+  session({
+    secret: "asadlfkj!@#!@#dfgasdg",
+    resave: false,
+    saveUninitialized: true,
+  })
 );
 
 app.get("/", async (req, res) => {
-    const rows = await db("SELECT * FROM user", []);
-    const user1 = rows[0].id;
-    console.log(user1);
-    res.send(rows);
+  const rows = await db("SELECT * FROM user", []);
+  const user1 = rows[0].id;
+  console.log(user1);
+  res.send(rows);
 });
 
-app.use("/api", user);
+app.use("/api/user", user);
 app.use("/api/board", board);
 app.use("/api/reply", reply);
 
 app.listen(app.get("port"), () => {
-    console.log("start");
+  console.log("start");
 });
 
 module.exports = app;
