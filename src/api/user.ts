03034a46f0918dcfc5e86cda55724672665ee811
cheol7826 <cheol7826@naver.com>
2021-05-11 18:00:19 +0900

    응답 데이터 변경 및 토큰인증 추가

--- src/api/user.ts
+++ src/api/user.ts
@@ -44,12 +44,12 @@ async function register(req: Request, res: Response) {
           schoolclass,
         ]
       );
-      res.send({ msg: "회원가입 성공" });
+      res.send({ success: true });
     } else {
-      res.status(400).send({ msg: "이미 존재하는 ID" });
+      res.status(400).send({ success: false });
     }
   } catch (error) {
-    res.status(500).send({ error: "에러" });
+    res.status(500).send({success: false});
   }
 }
 
@@ -62,11 +62,12 @@ async function login(req: Request, res: Response) {
   try {
     const { id, password } = loginScheme.validateSync(req.body);
     const rows = await db("SELECT * FROM user WHERE id=?", [id]);
-    if (!rows[0]) res.status(400).send("잘못된 ID입니다.");
+    if (!rows[0]) res.status(400).send({success: false});
     else if (await argon2.verify(rows[0].password, password)) {
       req.session.userId = id;
       req.session.password = password;
       req.session.isLogedIn = true;
+
       console.log({
         id: req.session.userId,
         password: req.session.password,
@@ -74,11 +75,16 @@ async function login(req: Request, res: Response) {
       });
 
       const token = await tokens.createTokens(id);
-      res.send({token: token, msg:"로그인 성공"});
+      res.send({
+        success: true,
+        token: token
+        });
     } else {
-      res.status(400).send("잘못된 Password입니다.");
+      res.status(400).send({success: false});
     }
-  } catch (error) {}
+  } catch (error) {
+      res.status(500).send({success: false})
+  }
 }
 
 async function logout(req: Request, res: Response) {
@@ -86,15 +92,15 @@ async function logout(req: Request, res: Response) {
   req.session.destroy((err) => {
     if (err) throw err;
   });
-  res.send({ msg: "로그아웃 했습니다." });
+  res.send({success: true});
 }
 
 async function userOut(req: Request, res: Response) {
   try {
     const rows = await db("DELETE FROM user WHERE id=?", [req.session.userId]);
-    res.send({ msg: "탈퇴성공" });
+    res.send({ success: true });
   } catch (error) {
-    res.status(500).send({ error: "탈퇴하는데 실패했습니다." });
+    res.status(500).send({ success: false });
   }
 }
 
@@ -106,7 +112,7 @@ async function confirmDupName(req: Request, res: Response) {
     }
     res.send({ success: true });
   } catch (error) {
-    res.status(500).send({ error: "탈퇴하는데 실패했습니다." });
+    res.status(500).send({success: false});
   }
 }
 
@@ -120,7 +126,7 @@ async function confirmDupNickname(req: Request, res: Response) {
     }
     res.send({ success: true });
   } catch (error) {
-    res.status(500).send({ error: "탈퇴하는데 실패했습니다." });
+    res.status(500).send({ success: false });
   }
 }
 
