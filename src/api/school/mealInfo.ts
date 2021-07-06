12f54146fa09213622afb314d039b140323b5bbf
KimYeeun99 <kyz0608@naver.com>
2021-07-06 13:25:34 +0900

    school부분 body->query 변경

--- src/api/school/mealInfo.ts
+++ src/api/school/mealInfo.ts
@@ -2,31 +2,27 @@ import { Router, Response, Request } from "express";
 import Neis from "@my-school.info/neis-api";
 
 const neis = new Neis({
-    KEY: process.env.NEIS_KEY,
-    Type: "json",
+  KEY: process.env.NEIS_KEY,
+  Type: "json",
 });
 
 async function getCafeteria(req: Request, res: Response) {
-    try {
-        const school = await neis.getSchoolInfo({
-            SCHUL_NM: "상명고등학교",
-        });
+  try {
+    const school = await neis.getSchoolInfo({
+      SCHUL_NM: "상명고등학교",
+    });
 
-        const mealInfo = await neis.getMealInfo({
-            ATPT_OFCDC_SC_CODE: school[0].ATPT_OFCDC_SC_CODE,
-            SD_SCHUL_CODE: school[0].SD_SCHUL_CODE,
-            MLSV_FROM_YMD: req.body.MLSV_FROM_YMD, //시작 일자
-            MLSV_TO_YMD: req.body.MLSV_TO_YMD, //종료 일자
-        });
+    const mealInfo = await neis.getMealInfo({
+      ATPT_OFCDC_SC_CODE: school[0].ATPT_OFCDC_SC_CODE,
+      SD_SCHUL_CODE: school[0].SD_SCHUL_CODE,
+      MLSV_FROM_YMD: req.query.MLSV_FROM_YMD as string, //시작 일자
+      MLSV_TO_YMD: req.query.MLSV_TO_YMD as string, //종료 일자
+    });
 
-        res.send({ mealInfo, success: true });
-    } catch (error) {
-        res.status(500).send({ error: error.message, success: false });
-    }
+    res.send({ mealInfo, success: true });
+  } catch (error) {
+    res.status(500).send({ error: error.message, success: false });
+  }
 }
 
 export { getCafeteria };
-// const router = Router();
-// router.get("/cafeteria", getCafeteria);
-
-// export default router;
