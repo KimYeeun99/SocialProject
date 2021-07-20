de5d8871d02e2d71925b2a4687087beb6b4aedc1
Nahui068 <kyjjj105@naver.com>
2021-07-20 20:35:37 +0900

    자신의 신고목록 조회 수정

--- src/api/board/report.ts
+++ src/api/board/report.ts
@@ -2,58 +2,69 @@ import { Response, Request } from "express";
 import * as yup from "yup";
 import { db } from "../../db/db";
 import tokens from "../common/token";
+import moment from "moment";
 
 export const reportScheme = yup.object({
-    board_id: yup.string().required(),
-    recv_id: yup.string().required(), //게시판 글쓴이
-    body: yup.string().required(), //신고내용
+  board_id: yup.string().required(),
+  recv_id: yup.string().required(), //게시판 글쓴이
+  body: yup.string().required(), //신고내용
 });
+
+function formatDate(date) {
+  return moment(date).format("YYYY-MM-DD HH:mm:ss");
+}
 
 //신고하기
 async function reportBoard(req: Request, res: Response) {
-    try {
-        const { board_id, recv_id, body } = reportScheme.validateSync(req.body);
+  try {
+    const { board_id, recv_id, body } = reportScheme.validateSync(req.body);
 
-        const rows = await db(
-            "INSERT INTO report (board_id, recv_id, send_id, body) values (?, ?, ?, ?)",
-            [board_id, recv_id, req.body.data.id, body]
-        );
+    const rows = await db(
+      "INSERT INTO report (board_id, recv_id, send_id, body) values (?, ?, ?, ?)",
+      [board_id, recv_id, req.body.data.id, body]
+    );
 
-        res.send({ success: true });
-    } catch (err) {
-        res.status(500).send({ success: false });
-    }
+    res.send({ success: true });
+  } catch (err) {
+    res.status(500).send({ success: false });
+  }
 }
 
 //자신이 신고한 신고목록 조회
 async function getReportById(req: Request, res: Response) {
-    try {
-        const user_id = req.body.data.id;
-        const report = await db("SELECT * FROM report WHERE user_id=?", [
-            user_id,
-        ]);
+  try {
+    const user_id = req.body.data.id;
+    const rows = await db("SELECT * FROM report WHERE send_id=?", [user_id]);
 
-        res.send({ report, success: true });
-    } catch (err) {
-        res.status(500).send({ success: false });
-    }
+    const data = JSON.parse(JSON.stringify(rows));
+    const report = [];
+
+    data.forEach((value) => {
+      value.regdate = formatDate(value.regdate);
+      report.push(value);
+    });
+
+    res.send({ report, success: true });
+  } catch (err) {
+    res.status(500).send({ success: false });
+  }
 }
 
 async function countReportById(req: Request, res: Response) {
-    try {
-        if (req.body.data.role === "master") {
-            const count = await db(
-                "SELECT recv_id, count(*) as count FROM report GROUP BY recv_id",
-                []
-            );
+  try {
+    if (req.body.data.role === "master") {
+      const count = await db(
+        "SELECT recv_id, count(*) as count FROM report GROUP BY recv_id",
+        []
+      );
 
-            res.send({ count, success: true });
-        } else {
-            res.status(400).send({ success: false });
-        }
-    } catch (err) {
-        res.status(500).send({ success: false });
+      res.send({ count, success: true });
+    } else {
+      res.status(400).send({ success: false });
     }
+  } catch (err) {
+    res.status(500).send({ success: false });
+  }
 }
 
 export { reportBoard, getReportById, countReportById };
