cc8e38b193853ec8e4ae5d1b102e41c0325cd511
cheol7826 <cheol7826@naver.com>
2021-07-27 21:39:20 +0900

    댓글 신고 기능 추가

--- src/api/reply/router.ts
+++ src/api/reply/router.ts
@@ -9,6 +9,7 @@ import {
     insertReply,
 } from "./reply";
 import { goodCount, goodReply } from "./good";
+import { reportReply, getMyReplyReport, getReplyReportById, countReplyReportById } from './report'
 
 const router = Router();
 
@@ -18,6 +19,12 @@ router.get("/good/:replyid", tokens.loginCheck, goodReply);
 
 //댓글 갯수
 router.get("/replycount/:boardid", replyCount);
+
+//댓글 신고 기능
+router.post("/report", tokens.loginCheck, reportReply);
+router.get("/report/me", tokens.loginCheck, getMyReplyReport);
+router.get("/report/count", tokens.loginCheck, countReplyReportById);
+router.get("/report", tokens.loginCheck, getReplyReportById);
 
 // 댓글 CRUD
 router.post("/:boardid", tokens.loginCheck, insertReply);
