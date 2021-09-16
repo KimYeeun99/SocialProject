757056dd439183bc4cfa5fd52d43fcc136bd9ca0
cheol7826 <cheol7826@naver.com>
2021-09-16 14:50:59 +0900

    시간표 기능 수정

--- src/api/school/timetable.ts
+++ src/api/school/timetable.ts
@@ -5,7 +5,9 @@ import * as yup from 'yup';
 export const timeTableScheme = yup.object({
     subject : yup.string().required(),
     days : yup.string().required(),
-    period : yup.number().required()
+    period : yup.number().required(),
+    location : yup.string().required(),
+    teacher : yup.string().required()
 })
 
 
@@ -20,17 +22,17 @@ async function insertTimeTable(req: Request, res: Response){
 
 
         list.forEach(async e => {
-            const {subject, days, period} = timeTableScheme.validateSync(e);
+            const {subject, days, period, location, teacher} = timeTableScheme.validateSync(e);
 
             const rows = await conn.query("SELECT user_id FROM timetable WHERE user_id=? AND days=? AND period=?",[user_id, days, period]);
             const check = JSON.parse(JSON.stringify(rows[0])); 
             
             if(!check[0]){
-                await conn.query("INSERT INTO timetable (user_id, subject, days, period) VALUES(?, ?, ?, ?)",
-                [user_id, subject, days, period]);
+                await conn.query("INSERT INTO timetable (user_id, subject, days, period, location, teacher) VALUES(?, ?, ?, ?, ? ,?)",
+                [user_id, subject, days, period, location, teacher]);
             } else{
-                await conn.query("UPDATE timetable set subject=? WHERE days=? AND period=? AND user_id =?",
-                [subject, days, period, user_id]);
+                await conn.query("UPDATE timetable set subject=?, location=?, teacher=? WHERE days=? AND period=? AND user_id =?",
+                [subject, location, teacher, days, period, user_id]);
             }
         })
         await conn.commit();
@@ -60,7 +62,11 @@ async function getTimeTable(req: Request, res: Response){
 
             const times = "t" + e.period;
 
-            table[e.days][times] = e.subject;
+            table[e.days][times] = {
+                subject: e.subject,
+                location : e.location,
+                teacher : e.teacher
+            };
         })
         res.json({table : table});
     } catch(err){
@@ -81,9 +87,8 @@ async function deleteTimeTable(req: Request, res: Response) {
 
         list.forEach(async element => {
             try{
-                const {subject, days, period} = timeTableScheme.validateSync(element);
                 await conn.query("DELETE FROM timetable WHERE user_id=? AND days=? AND period=?",
-                [user_id, days, period]);
+                [user_id, element.days, element.period]);
             } catch(err){
                 await conn.rollback();
                 res.status(500).send({success: false});
