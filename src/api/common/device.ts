aa42da1f5fe95cc413dce078045a3b5f38f16023
KimYeeun99 <kyz0608@naver.com>
2021-11-07 23:16:08 +0900

    장치 삭제 기능 추가

--- src/api/common/device.ts
+++ src/api/common/device.ts
@@ -1,52 +1,59 @@
 import { Request, Response } from "express";
 import { pool } from "../../db/db";
-import {logger} from "../../log/logger";
+import { logger } from "../../log/logger";
 // 장치 등록
-async function insertDevice(req: Request, res: Response){
-    try{
+async function insertDevice(req: Request, res: Response) {
+    try {
         const deviceToken = req.body.deviceToken;
         const userId = req.body.data.id;
         const curDevice = await getDevice(userId);
 
-
-        if(curDevice == ""){
-            await pool.query("INSERT INTO devices VALUES(?, ?)", [userId, deviceToken]);
+        if (curDevice == "") {
+            await pool.query("INSERT INTO devices VALUES(?, ?)", [
+                userId,
+                deviceToken,
+            ]);
             logger.info(`New Device Added/${userId}/${deviceToken}`);
         }
 
-        if(curDevice != deviceToken){
-            await pool.query("UPDATE devices SET device_id=? WHERE user_id=?", [deviceToken, userId]);
+        if (curDevice != deviceToken) {
+            await pool.query("UPDATE devices SET device_id=? WHERE user_id=?", [
+                deviceToken,
+                userId,
+            ]);
             logger.info(`Changed Device/${userId}/${deviceToken}`);
         }
 
-        res.json({success: true});
-    } catch(error){
+        res.json({ success: true });
+    } catch (error) {
         logger.error("[insertDevice]" + error);
-        res.status(500).send({success: false});
+        res.status(500).send({ success: false });
     }
 }
 
 // 장치 조회
-async function getDevice(userId : string){
-    try{
-        const rows = await pool.query("SELECT device_id FROM devices WHERE user_id=?", [userId]);
+async function getDevice(userId: string) {
+    try {
+        const rows = await pool.query(
+            "SELECT device_id FROM devices WHERE user_id=?",
+            [userId]
+        );
         const check = JSON.parse(JSON.stringify(rows[0]));
 
-        if(!check[0]){
+        if (!check[0]) {
             return "";
         }
 
         return check[0].device_id;
-    } catch(error){
+    } catch (error) {
         logger.error("[getDevice]" + error);
         throw error;
     }
-    
 }
 
 // 장치 전체 조회
-async function getAllDevice(){
-    try{
+async function getAllDevice() {
+    try {
         const rows = await pool.query("SELECT device_id FROM devices", []);
         const check = JSON.parse(JSON.stringify(rows[0]));
 
@@ -57,10 +64,25 @@ async function getAllDevice(){
         }
         console.log(list);
         return list;
-    } catch(error){
+    } catch (error) {
         logger.error("[getAllDevice]" + error);
         throw error;
     }
 }
 
-export {insertDevice, getDevice, getAllDevice};
+// 장치 등록 삭제
+async function deleteDevice(req: Request, res: Response) {
+    try {
+        const userId = req.body.data.id;
+
+        await pool.query("DELETE FROM devices WHERE user_id=?", [userId]);
+        logger.info(`Delete Device/${userId}`);
+
+        res.json({ success: true });
+    } catch (error) {
+        logger.error("[deleteDevice]" + error);
+        res.status(500).send({ success: false });
+    }
+}
+
+export { insertDevice, getDevice, getAllDevice, deleteDevice };
--- src/app.ts
+++ src/app.ts
@@ -6,9 +6,9 @@ import reply from "./api/reply/router";
 import school from "./api/school/router";
 import token from "./api/common/token";
 import notice from "./api/notice/router";
-import { insertDevice } from "./api/common/device";
+import { insertDevice, deleteDevice } from "./api/common/device";
 import "dotenv/config";
-import {logger} from "./log/logger";
+import { logger } from "./log/logger";
 
 const app = express();
 
@@ -23,16 +23,17 @@ app.use("/api/reply", reply);
 app.use("/api/school", school);
 app.use("/api/notice", notice);
 
-
 // 토큰 인증
 app.post("/api/auth/refresh", token.tokenRefresh);
 app.get("/api/auth/valid", token.validCheck);
 
 // 장치 등록
 app.post("/api/device", token.loginCheck, insertDevice);
+// 장치 삭제
+app.delete("/api/device", token.loginCheck, deleteDevice);
 
 app.listen(app.get("port"), () => {
-    logger.info("SocialServer Start")
+    logger.info("SocialServer Start");
     console.log("start");
 });
 
